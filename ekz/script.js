1817ad1213ea4c55dfb4af2e7573293a5cdc0640
AndrianHaliuk <163265843+AndrianHaliuk@users.noreply.github.com>
2024-12-07 08:25:53 +0100

    Add files via upload

---
+++ ekz/script.js
@@ -0,0 +1,12 @@
+const input = document.getElementById('numberInput');
+
+input.addEventListener('blur', () => {
+    const value = parseInt(input.value, 10);
+    if (!isNaN(value) && value >= 1 && value <= 100) {
+        input.classList.add('green');
+        input.classList.remove('red');
+    } else {
+        input.classList.add('green');
+        input.classList.remove('red');
+    }
+});
